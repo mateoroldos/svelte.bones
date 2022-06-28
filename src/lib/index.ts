5def3f8b72c4ba0699244c5af4f1455da60d325d
mateoroldos <mateoroldos19@gmail.com>
2022-06-28 15:47:43 -0300

    feat: :sparkles: AdaptableGrid component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,5 @@ export { default as Section } from './components/Section.svelte';
 export { default as Container } from './components/Container.svelte';
 export { default as Row } from './components/Row.svelte';
 export { default as Column } from './components/Column.svelte';
+export { default as Card } from './components/Card.svelte';
+export { default as AdaptableGrid } from './components/AdaptableGrid.svelte';
